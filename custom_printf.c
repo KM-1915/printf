d3efed0dec1c4bcb58054ac85a6226200f4c6cb0
Jacqualine <khomotso.mmogoba@gmail.com>
2023-08-20 21:56:51 -0700

    printf

--- custom_printf.c
+++ custom_printf.c
@@ -1,52 +1,105 @@
 #include "main.h"
 #include<stdarg.h>
-/**
- * _printf - produces output according to a format
- * @format: a character string
- * Return: number of characters printed
- * excluding the null byte used to end output to strings
- */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int print_char(va_list args, char *buffer, int *index)
+{
+	char c = va_arg(args, int);
+
+	buffer[*index] = c;
+	(*index)++;
+	return (1);
+}
+
+int print_string(va_list args, char *buffer, int *index)
+{
+	char *str = va_arg(args, char *);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		buffer[*index] = str[i];
+		(*index)++;
+		i++;
+	}
+	return (i);
+}
+
+int (*get_op_func(const char *format))(va_list, char *, int *)
+{
+	int i = 0;
+
+	print_t print_arr[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{NULL, NULL}
+	};
+	while (print_arr[i].type != NULL)
+	{
+		if (*(print_arr[i].type) == *format)
+		{
+			return (print_arr[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
 int _printf(const char *format, ...)
 {
-	int c, num = 0;
-	char *s;
 	va_list args;
+	int i = 0, j = 0, printed_chars = 0;
+	char *buffer;
+	int (*function)(va_list, char *, int *);
 
 	va_start(args, format);
-	while (*format != '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 	{
-		if (*format == '%')
+		return (-1);
+	}
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		return (-1);
+	}
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
 		{
-			format++;
-			if (*format == '%')
+			if (format[i + 1] == '\0')
 			{
-				putchar('%');
-				num++;
+				return (-1);
 			}
-			else if (*format == 'c')
+			function = get_op_func(&format[i + 1]);
+			if (function == NULL)
 			{
-				c = va_arg(args, int);
-				putchar(c);
-				num++;
+				buffer[j] = format[i];
+				j++;
+				printed_chars++;
 			}
-			else if (*format == 's')
+			else
 			{
-				s = va_arg(args, char*);
-				while (*s != '\0')
-				{
-					putchar(*s);
-					s++;
-					num++;
-				}
+				i++;
+				printed_chars += function(args, buffer, &j);
 			}
 		}
 		else
 		{
-			putchar(*format);
-			count++;
+			buffer[j] = format[i];
+			j++;
+			printed_chars++;
 		}
-		format++;
+		i++;
 	}
+	write(1, buffer, j);
+	free(buffer);
 	va_end(args);
-	return (num);
+	return (printed_chars);
 }
+
